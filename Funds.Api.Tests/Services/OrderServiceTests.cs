46c29fcfb5676929e16de83fd5513eb1e4e3196c
Daniel Melo <danimelo.dev@gmail.com>
2026-05-09 23:58:40 -0300

    test: add unit test project setup

---
+++ Funds.Api.Tests/Services/OrderServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+
+namespace Funds.Api.Tests.Services;
+
+public class OrderServiceTests
+{
+    [Fact]
+    public void Should_Run_First_Test()
+    {
+        var result = 1 + 1;
+
+        result.Should().Be(2);
+    }
+}
