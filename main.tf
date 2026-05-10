d34917431d0fd2f89935dafe1d4531f8c786fcdd
Daniel Melo <danimelo.dev@gmail.com>
2026-05-10 02:22:27 -0300

    feat: add terraform ecr infrastructure

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_ecr_repository" "funds_api" {
+  name                 = var.ecr_repository_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Project = "FundsApi"
+    Environment = "Development"
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "ecr_repository_url" {
+  value = aws_ecr_repository.funds_api.repository_url
+}
